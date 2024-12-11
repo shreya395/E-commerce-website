da7f2fdad474d3d4a26bcfde0362f76e96a1a115
Shreya Shukla <101269825+shreya395@users.noreply.github.com>
2024-12-11 13:12:25 +0530

    Update cloudinary.js

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dxg5t66od",
-  api_key: "923775896254898",
-  api_secret: "S4-hc84_gV0bQ60uDQ62jtNmefI",
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
 });
 
 const storage = new multer.memoryStorage();
